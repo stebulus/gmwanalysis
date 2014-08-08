870d6a1bbafc9397849b8010acd745958ef9dd2a
Steven Taschuk <steven@amotlpaa.org>
2014-08-08 14:15:39 -0400

    Store population and sample in maps, in analyze.hs

--- analyze.hs
+++ analyze.hs
@@ -1,6 +1,8 @@
 import Control.Applicative
 import Data.Function
 import Data.List
+import Data.Map (Map, elems)
+import qualified Data.Map as M
 import qualified Data.Set as S
 import System.Environment
 
@@ -18,45 +20,52 @@ instance Show (Feature a b) where
     show f = "f\"" ++ tag f ++ "\""
 
 data Model a b = Model { features :: [Feature a b]
-                       , population :: [a]
-                       , sample :: [a]
+                       , classedPop :: Map [b] [a]
+                       , classedSamp :: Map [b] [a]
                        }
 
-nullModel :: [a] -> [a] -> Model a b
-nullModel = Model []
+population :: Ord b => Model a b -> [a]
+population = concat . elems . classedPop
+
+sample :: Ord b => Model a b -> [a]
+sample = concat . elems . classedSamp
+
+nullModel :: Ord b => [a] -> [a] -> Model a b
+nullModel = Model [] `on` (\v -> M.insert [] v M.empty)
 
 classify :: Model a b -> a -> [b]
 classify model x = [func f x | f <- features model]
 
-sampleSize :: Model a b -> Int
+sampleSize :: Ord b => Model a b -> Int
 sampleSize = length . sample
 
-classSizePop :: Eq b => Model a b -> [b] -> Int
+classSizePop :: (Eq b, Ord b) => Model a b -> [b] -> Int
 classSizePop model cls =
     length $ filter (== cls) $ map (classify model) $ population model
 
-classSizeSamp :: Eq b => Model a b -> [b] -> Int
+classSizeSamp :: (Eq b, Ord b) => Model a b -> [b] -> Int
 classSizeSamp model cls =
     length $ filter (== cls) $ map (classify model) $ sample model
 
 refine :: Model a b -> Feature a b -> Model a b
 refine model f = model { features = f:(features model) }
 
-weight :: (Eq b, Fractional c) => Model a b -> a -> c
+weight :: (Eq b, Ord b, Fractional c) => Model a b -> a -> c
 weight model x =
     classSizeSamp model cls // ((classSizePop model cls) * (sampleSize model))
     where cls = classify model x
 
-sampleLikelihood :: (Eq b, Floating c) => Model a b -> c
+sampleLikelihood :: (Eq b, Ord b, Floating c) => Model a b -> c
 sampleLikelihood model =
     sum $ map log $ map (weight model) $ sample model
 
-refineBest :: Eq b => (Model a b,[Feature a b]) -> (Model a b,[Feature a b])
+refineBest :: (Eq b, Ord b) =>
+    (Model a b,[Feature a b]) -> (Model a b,[Feature a b])
 refineBest (model, features) =
     maximumBy (compare `on` (sampleLikelihood.fst))
               [(refine model f, fs) | (f,fs) <- picks features]
 
-refinements :: Eq b => Model a b -> [Feature a b] -> [Model a b]
+refinements :: (Eq b, Ord b) => Model a b -> [Feature a b] -> [Model a b]
 refinements model features =
     map fst
     $ takeWhile (not . null . snd)
