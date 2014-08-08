748e2b18e55e9d0ee0ab872ef41f54bc8505f3d6
Steven Taschuk <steven@amotlpaa.org>
2014-08-08 14:37:27 -0400

    Enlarge testing sample to 20000.

--- analyze.hs
+++ analyze.hs
@@ -88,7 +88,7 @@ refinements model features =
 
 main = do
     n <- fmap (read . (!!0)) getArgs :: IO Int
-    allwords <- fmap ((take 5000) . lines) $ readFile "twl"
+    allwords <- fmap ((take 20000) . lines) $ readFile "twl"
     let allwordsSet = S.fromList allwords
     chosenwords <-
         fmap (filter (`S.member` allwordsSet))
